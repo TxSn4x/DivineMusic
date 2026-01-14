bb562f49fae1b43962761bb93d1f0c06d2d1623d
TxSn4x <txsn4x@gmail.com>
2026-01-15 03:24:17 +0530

    Add files via upload

--- SONALI/utils/inline/play.py
+++ SONALI/utils/inline/play.py
@@ -76,10 +76,10 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text=" 𝗝ᴏɪɴ 🤌 ", url="https://t.me/PURVI_BOTS",
+                text=" Uᴘᴅᴀᴛᴇs ", url="https://t.me/aniweb_bots",
             ),
             InlineKeyboardButton(
-                text="🌸 𝗦ᴜᴘᴘᴏʀᴛ 🌸", url="https://t.me/PURVI_UPDATES",
+                text=" Sᴜᴘᴘᴏʀᴛ ", url="https://t.me/aniweb_nexus",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -99,10 +99,10 @@ def stream_markup(_, videoid, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="𝗝ᴏɪɴ 🤌", url="https://t.me/PURVI_BOTS",
+                text=" Uᴘᴅᴀᴛᴇs ", url="https://t.me/aniweb_bots",
             ),
             InlineKeyboardButton(
-                text="🌸 𝗦ᴜᴘᴘᴏʀᴛ 🌸", url="https://t.me/PURVI_UPDATES",
+                text=" Sᴜᴘᴘᴏʀᴛ ", url="https://t.me/aniweb_nexus",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
