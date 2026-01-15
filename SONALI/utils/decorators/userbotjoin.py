235ed285ed0ad9200774162a0f680ce698413e70
TxSn4x <txsn4x@gmail.com>
2026-01-15 11:13:02 +0530

    Update userbotjoin.py

--- SONALI/utils/decorators/userbotjoin.py
+++ SONALI/utils/decorators/userbotjoin.py
@@ -85,24 +85,24 @@ def UserbotWrapper(command):
                             invitelink = await app.export_chat_invite_link(chat_id)
                         except ChatAdminRequired:
                             return await message.reply_text(
-                                "➥ 𝗣𝐥𝐞𝐚𝐬𝐞  𝗠𝐚𝐤𝐞  𝗠𝐞  𝗔𝐝𝐦𝐢𝐧 𝗔𝐧𝐝 𝗠𝐮𝐬𝐭  𝗚𝐢𝐯𝐞  𝗜𝐧𝐯𝐢𝐭𝐞  𝗨𝐬𝐞𝐫𝐬  𝗣𝐨𝐰𝐞𝐫  𝗙𝐨𝐫  𝗜𝐧𝐯𝐢𝐭𝐞  𝗠𝐲 𝗔𝐬𝐬𝐢𝐬𝐭𝐚𝐧𝐭  𝗜𝐧  𝗧𝐡𝐢𝐬  𝗖𝐡𝐚𝐭."
+                                "➥ ᴘʟᴇᴀsᴇ ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ ᴀɴᴅ ɢɪᴠᴇ ɪɴᴠɪᴛᴇᴜsᴇʀ ʀɪɢʜᴛs ."
                             )
                         except Exception as e:
                             return await message.reply_text(
-                                f"{app.mention} 𝗔𝘀𝘀𝗶𝘀𝘁𝗮𝗻𝘁 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝗝𝗼𝗶𝗻𝗲𝗱 𝗧𝗵𝗶𝘀 𝗚𝗿𝗼𝘂𝗽✅\n\n𝗜𝗱:- {userbot.mention}.."
+                                f"{app.mention} ᴀssɪsᴛᴀɴᴛ sᴜᴄᴄᴇsғᴜʟʟʏ ᴊᴏɪɴᴅ ᴛʜᴇ ᴄʜᴀᴛ \n\nɪᴅ:- {userbot.mention}.."
                             )
 
                 if invitelink.startswith("https://t.me/+"):
                     invitelink = invitelink.replace(
                         "https://t.me/+", "https://t.me/joinchat/"
                     )
-                myu = await message.reply_text("𝗔𝘀𝘀𝗶𝘀𝘁𝗮𝗻𝘁 𝗝𝗼𝗶𝗻𝗶𝗻𝗴 𝗧𝗵𝗶𝘀 𝗖𝗵𝗮𝘁..")
+                myu = await message.reply_text("ᴀssɪsᴛᴀɴᴅ ᴊᴏɪɴɪɴɢ ᴛʜɪs ᴄʜᴀᴛ..")
                 try:
                     await asyncio.sleep(1)
                     await userbot.join_chat(invitelink)
                     await myu.delete()
                     await message.reply_text(
-                        f"{app.mention} 𝗔𝘀𝘀𝗶𝘀𝘁𝗮𝗻𝘁 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝗝𝗼𝗶𝗻𝗲𝗱 𝗧𝗵𝗶𝘀 𝗚𝗿𝗼𝘂𝗽✅\n\n𝗜𝗱:- **@{userbot.username}**"
+                        f"{app.mention} ᴀssɪsᴛᴀɴᴛ sᴜᴄᴄᴇsғᴜʟʟʏ ᴊᴏɪɴᴅ ᴛʜᴇ ᴄʜᴀᴛ\n\nɪᴅ:- **@{userbot.username}**"
                     )
                 except InviteRequestSent:
                     try:
@@ -114,13 +114,13 @@ def UserbotWrapper(command):
                     await asyncio.sleep(3)
                     await myu.delete()
                     await message.reply_text(
-                        f"{app.mention} 𝗔𝘀𝘀𝗶𝘀𝘁𝗮𝗻𝘁 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝗝𝗼𝗶𝗻𝗲𝗱 𝗧𝗵𝗶𝘀 𝗚𝗿𝗼𝘂𝗽✅\n\n𝗜𝗱:- **@{userbot.username}**"
+                        f"{app.mention} ᴀssɪsᴛᴀɴᴛ sᴜᴄᴄᴇsғᴜʟʟʏ ᴊᴏɪɴᴅ ᴛʜᴇ ᴄʜᴀᴛ\n\n𝗜𝗱:- **@{userbot.username}**"
                     )
                 except UserAlreadyParticipant:
                     pass
                 except Exception as e:
                     return await message.reply_text(
-                        f"{app.mention} 𝗔𝘀𝘀𝗶𝘀𝘁𝗮𝗻𝘁 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝗝𝗼𝗶𝗻𝗲𝗱 𝗧𝗵𝗶𝘀 𝗚𝗿𝗼𝘂𝗽✅\n\n𝗜𝗱:- **@{userbot.username}**"
+                        f"{app.mention} ᴀssɪsᴛᴀɴᴛ sᴜᴄᴄᴇsғᴜʟʟʏ ᴊᴏɪɴᴅ ᴛʜᴇ ᴄʜᴀᴛ\n\n𝗜𝗱:- **@{userbot.username}**"
                     )
 
                 links[chat_id] = invitelink
