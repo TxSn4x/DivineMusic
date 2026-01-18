5f0b44afc793382f05224d9f7f61cf743b0b2a75
TxSn4x <txsn4x@gmail.com>
2026-01-18 20:38:58 +0530

    Update stream.py

--- SONALI/utils/stream/stream.py
+++ SONALI/utils/stream/stream.py
@@ -22,23 +22,7 @@ from SONALI.utils.stream.queue import put_queue, put_queue_index
 from SONALI.utils.thumbnails import get_thumb
 
 # Authored By Certified Coders © 2025
-import os
-from random import randint
-from typing import Union
 
-from pyrogram.types import InlineKeyboardMarkup
-
-import config
-from SONALI import Carbon, YouTube, app
-from SONALI.core.call import StreamController
-from SONALI.misc import db
-from SONALI.utils.database import add_active_video_chat, is_active_chat
-from SONALI.utils.exceptions import AssistantErr
-from SONALI.utils.inline import aq_markup, close_markup, stream_markup
-from SONALI.utils.pastebin import ANNIEBIN
-from SONALI.utils.stream.queue import put_queue, put_queue_index
-from SONALI.utils.thumbnails import get_thumb
-from SONALI.utils.errors import capture_internal_err
 
 
 @capture_internal_err
